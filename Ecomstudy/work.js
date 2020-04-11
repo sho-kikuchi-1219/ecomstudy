88d2035db18b9e4bc6b654d5ad4e530dea862e85
sho-kikuchi <ecomkikuchi@gmail.com>
2020-04-12 01:29:01 +0900

    addCell

--- Ecomstudy/work.js
+++ Ecomstudy/work.js
@@ -1,2 +1,8 @@
 let random = 1 + Math.floor(Math.random()*16);
 console.log(random);
+
+let table = document.getElementById('targetTable');
+let newRow = table.insertRow();
+let newCell = newRow.insertCell();
+let newText = document.createTextNode("あ");
+newCell.appendChild(newText);
